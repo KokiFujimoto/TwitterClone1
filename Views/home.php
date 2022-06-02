38194b95ace188f525fa425e4d2131ee2d6009a8
Fujimoto <gegvp0330@gmail.com>
2022-06-02 16:48:15 +0900

    headタグ作成

---
+++ Views/home.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../Views/img/logo-twitterblue.svg">
+    <link href="../Views/css/style.css" rel="stylesheet">
+ 
+    <title>ホーム画面 / Twitterクローン</title>
+    <meta name="description" content="ホーム画面です">
+</head>
+ 
+<body>
+    テスト
+</body>
+ 
+</html>
