d78a806019939f7f376408dc8b8dc60d898f7724
Fujimoto <gegvp0330@gmail.com>
2022-06-02 16:57:16 +0900

    つぶやき一覧エリアの見た目

--- Views/home.php
+++ Views/home.php
@@ -14,7 +14,7 @@
     <meta name="description" content="ホーム画面です">
 </head>
  
-<body>
+<body class="home">
     <div class="container">
         <div class="side">
             <div class="side-inner">
@@ -32,6 +32,77 @@
         <div class="main">
             <div class="main-header">
                 <h1>ホーム</h1>
+            </div>
+ 
+            <!-- つぶやき投稿エリア -->
+            <div class="tweet-post">
+                <div class="my-icon">
+                    <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
+                </div>
+                <div class="input-area">
+                    <form action="post.php" method="post" enctype="multipart/form-data">
+                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
+                        <div class="bottom-area">
+                            <div class="mb-0">
+                                <input type="file" name="image" class="form-control form-control-sm">
+                            </div>
+                            <button class="btn" type="submit">つぶやく</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+ 
+            <!-- 仕切りエリア -->
+            <div class="ditch"></div>
+ 
+            <!-- つぶやき一覧エリア -->
+            <div class="tweet-list">
+                <div class="tweet">
+                    <div class="user">
+                        <a href="profile.php?user_id=1">
+                            <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="content">
+                        <div class="name">
+                            <a href="profile.php?user_id=1">
+                                <span class="nickname">太郎</span>
+                                <span class="user-name">@taro ・23日前</span>
+                            </a>
+                        </div>
+                        <p>今プログラミングをしています。</p>
+                        <div class="icon-list">
+                            <div class="like">
+                                <img src="../Views/img/icon-heart.svg" alt="">
+                            </div>
+                            <div class="like-count">0</div>
+                        </div>
+                    </div>
+                </div>
+ 
+                <div class="tweet">
+                    <div class="user">
+                        <a href="profile.php?user_id=2">
+                            <img src="../Views/img/icon-default-user.svg" alt="">
+                        </a>
+                    </div>
+                    <div class="content">
+                        <div class="name">
+                            <a href="profile.php?user_id=2">
+                                <span class="nickname">次郎</span>
+                                <span class="user-name">@jiro ・24日前</span>
+                            </a>
+                        </div>
+                        <p>コワーキングスペースをオープンしました！</p>
+                        <img src="../Views/img_uploaded/tweet/sample-post.jpg" alt="" class="post-image">
+                        <div class="icon-list">
+                            <div class="like">
+                                <img src="../Views/img/icon-heart-twitterblue.svg" alt="">
+                            </div>
+                            <div class="like-count">1</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
